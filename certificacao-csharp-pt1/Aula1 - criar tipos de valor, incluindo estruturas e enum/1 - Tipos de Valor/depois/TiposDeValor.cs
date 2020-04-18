ec49e0d0267c143dd6b28f70b83ac124b58c3156
Daniel Pires <danielpires6@hotmail.com>
2020-04-18 17:28:32 -0300

    Aula 3 - Variáveis do tipo referência

--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/depois/TiposDeValor.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/depois/TiposDeValor.cs
@@ -10,35 +10,22 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
-            int idade = 15;
-            //idade = 15.5;
+            int idade;
+            idade = 30;
+            Console.WriteLine(idade);
 
-            char resposta = 'S';// System.Char
+            System.Int32 copiaIdade = idade;
 
-            byte nivelDeAzul = 0xF; // = 255 em decimal 
-                                    //nivelDeAzul = -3
-            short passageirosVoo = 230; // System.Int16
-            passageirosVoo = -7;
-            int populacao = 1500; // SystemInt.32
-            populacao = -2300;
-            long populacaoDoBrasil = 207_660_929; // +/- 207 milhões - System.Int64
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
 
-            sbyte niveldeBrilho = -127; //System.SByte
-            ushort passageirosNavio = 230; // System.UInt16 
-            uint estoque = 1500; // System.UInt32
-            ulong populacaoDoMundo = 7_000_000_000; //7 bilhões = System.UInt64
+            idade = 23;
 
-            Console.WriteLine($"resposta: {resposta}");
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
 
-            Console.WriteLine($"nivelDeAzul: {nivelDeAzul}");
-            Console.WriteLine($"passageirosVoo: {passageirosVoo}");
-            Console.WriteLine($"populacao: {populacao}");
-            Console.WriteLine($"populacaoDoBrasil: {populacaoDoBrasil}");
-
-            Console.WriteLine($"niveldeBrilho: {niveldeBrilho}");
-            Console.WriteLine($"estoque: {estoque}");
-            Console.WriteLine($"populacaoDoMundo: {populacaoDoMundo}");
-
+            int? idade2 = null;
+            System.Nullable<int> idade3 = null;
         }
     }
 }
--- certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/6 - Estruturas Definidas pelo Usuário/depois/Estruturas.cs
+++ certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/6 - Estruturas Definidas pelo Usuário/depois/Estruturas.cs
@@ -6,6 +6,28 @@ using System.Threading.Tasks;
 
 namespace certificacao_csharp_roteiro
 {
+    class Estruturas : IAulaItem
+    {
+        public void Executar()
+        {
+            double Latitude1 = 13.78;
+            double Longitude1 = 29.51;
+            double Latitude2 = 40.23;
+            double Longitude2 = 17.4;
+            Console.WriteLine($"Latitude1 = {Latitude1}");
+            Console.WriteLine($"Longitude1 = {Longitude1}");
+            Console.WriteLine($"Latitude2 = {Latitude2}");
+            Console.WriteLine($"Longitude2 = {Longitude2}");
+
+            PosicaoGPS posicao1;
+            posicao1.Latitude = 13.78;
+            posicao1.Longitude = 29.51;
+
+            posicao1 = new PosicaoGPS(13.78, 29.51);
+
+            Console.WriteLine(posicao1);
+        }
+    }
 
     interface IGPS
     {
@@ -13,29 +35,29 @@ namespace certificacao_csharp_roteiro
     }
 
     struct PosicaoGPS : IGPS
-            {
-                public double Latitude;
-                public double Longitude;
-
-                public PosicaoGPS(double latitude, double longitude)
-                {
-                    Latitude = latitude;
-                    Longitude = longitude;
-                }
-
-                public bool EstaNoHemisferioNorte()
-                {
-                    return Latitude > 0;
-                }
-            }
-
-    class Estruturas : IAulaItem
     {
-        public void Executar()
-        {
+        public double Latitude;
+        public double Longitude;
 
-            PosicaoGPS localizacao = new PosicaoGPS(23.6, 21.9);
-            Console.WriteLine(localizacao.Latitude);
+        //public PosicaoGPS()
+        //{
+
+        //}
+
+        public PosicaoGPS(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public bool EstaNoHemisferioNorte()
+        {
+            return Latitude > 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Latitude: {Latitude}, Longitude: {Longitude}";
         }
     }
 }
